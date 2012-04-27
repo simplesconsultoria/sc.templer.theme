f6ffc9878b1fddff83406dbbbacf9ea1c9e126b4
Cleber J Santos <cleber@cleberjsantos.com.br>
2012-04-27 14:17:38 -0300

    Fixes

--- sc/templer/theme/diazo.py
+++ sc/templer/theme/diazo.py
@@ -1,18 +1,12 @@
 # -*- coding:utf-8 -*-
 import copy
 
-from templer.core.base import BaseTemplate
-
-from templer.core.vars import DottedVar
-from templer.core.vars import StringVar
-
-from templer.core.vars import EASY
+from templer.core.base import get_var
 from templer.core.vars import EXPERT
 
-from sc.templer.core import DEFAULTS as D
+from sc.templer.core.base import PlonePackage
 
-
-class Diazo(BaseTemplate):
+class Diazo(PlonePackage):
 
     summary = "Create a Diazo theme based on beyondskins.responsive."
     help = """This template allows you to create a Diazo theme based on
@@ -20,87 +14,13 @@ class Diazo(BaseTemplate):
 
     category = "Simples Consultoria - Themes"
 
-    ndots = 1
-    use_cheetah = True
-
-    required_templates = []
-    default_required_structures = []
     _template_dir = "templates/diazo"
 
-    vars = copy.deepcopy(BaseTemplate.vars)
-    vars += [
-        StringVar(
-            'title',
-            title='Title',
-            description='Theme title',
-            default='Beyondskins Theme',
-            modes=(EASY, EXPERT),
-            page='Metadata',
-            help="""A friendly name for this theme."""),
+    vars = copy.deepcopy(PlonePackage.vars)
 
-        DottedVar(
-            'namespace_package',
-            title='Namespace Package Name',
-            description='Name of outer namespace package',
-            default='beyondskins',
-            modes=(EXPERT, ),
-            page='Namespaces',
-            help="""First level name for the diazo theme."""
-        ),
+    get_var(vars, 'plone_version').modes = (EXPERT, )
+    get_var(vars, 'keywords').default = 'plone theme simples_consultoria'
 
-        DottedVar(
-            'package',
-            title='Package Name',
-            description='Name of the inner namespace package',
-            default='theme',
-            modes=(EXPERT, ),
-            page='Namespaces',
-            help="""Second level name for the diazo theme."""),
-
-        StringVar(
-            'version',
-            title='Version',
-            description='Version number for project',
-            default='1.0',
-            modes=(EASY, EXPERT),
-            page='Metadata',
-            help="""Version number"""
-            ),
-
-        StringVar(
-            'description',
-            title='Description',
-            description='One-line description of the project',
-            default='Diazo Theme developed by Simples Consultoria',
-            modes=(EASY, EXPERT),
-            page='Metadata',
-            help="""Description of this theme."""
-            ),
-
-        StringVar(
-            'author',
-            title='Author',
-            description='Name of author for project',
-            default= D.get('author'),
-            modes=(),
-            page='Metadata',
-            help="""Name that will appear on the theme description"""
-            ),
-
-        StringVar(
-            'author_email',
-            title='Author Email',
-            description='Email of author for project',
-            default = D.get('email'),
-            modes=(),
-            page='Metadata',
-            help="""Email of the author of the theme."""
-            ),
-    ]
-
-    def check_vars(self, vars, command):
-        if not command.options.no_interactive and \
-           not hasattr(command, '_deleted_once'):
-            del vars['package']
-            command._deleted_once = True
-        return super(Diazo, self).check_vars(vars, command)
+    def check_vars(self, vars, cmd):
+        responses = super(Diazo, self).check_vars(vars, cmd)
+        return responses
