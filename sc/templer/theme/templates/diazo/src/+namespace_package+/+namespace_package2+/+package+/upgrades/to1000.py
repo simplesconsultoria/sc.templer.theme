1795ef16b808657c0cfddb53c76ad6e91e471867
Cleber J Santos <cleber@cleberjsantos.com.br>
2012-05-02 09:10:53 -0300

    Upgrades

--- sc/templer/theme/templates/diazo/src/+namespace_package+/+namespace_package2+/+package+/upgrades/to1000.py
+++ sc/templer/theme/templates/diazo/src/+namespace_package+/+namespace_package2+/+package+/upgrades/to1000.py
@@ -1,18 +1,14 @@
 # -*- coding: utf-8 -*-
 
 import logging
-
 from collective.grok import gs
 
-from beyondskins.cerelatino.site.config import DEPENDENCIES
-from beyondskins.cerelatino.site.config import PROJECTNAME
 
-
-@gs.upgradestep(title=u'Initial upgrade steo',
+@gs.upgradestep(title=u'Initial upgrade step',
                 description=u'Upgrade step run at install time',
                 source='0.0', destination='1000', sortkey=1,
                 profile='beyondskins.cerelatino.site:default')
-def fromZero(context):
+def to1000(context):
     """ Upgrade from Zero to version 1000
     """
     pass
