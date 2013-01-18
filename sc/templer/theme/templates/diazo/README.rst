9e6a3758f4d90128eb55f24daf79e9726801f392
Cleber J Santos <cleber@cleberjsantos.com.br>
2013-01-18 16:38:24 -0200

    Fix all templer of diazo themer

--- sc/templer/theme/templates/diazo/README.rst
+++ sc/templer/theme/templates/diazo/README.rst
@@ -1,1 +1,17 @@
-README.txt
+.. contents:: Table of Contents
+   :depth: 2
+
+*****************************************************
+${namespace_package}.${namespace_package2}.${package}
+*****************************************************
+
+Overview
+--------
+
+Please fill me...
+
+Requirements
+------------
+
+* Plone >= 4.1 (http://plone.org/products/plone)
+
--- sc/templer/theme/templates/diazo/src/+namespace_package+/+namespace_package2+/+package+/upgrades/to1000.py
+++ sc/templer/theme/templates/diazo/src/+namespace_package+/+namespace_package2+/+package+/upgrades/to1000.py
@@ -1,13 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import logging
-from collective.grok import gs
 
 
-@gs.upgradestep(title=u'Initial upgrade step',
-                description=u'Upgrade step run at install time',
-                source='0.0', destination='1000', sortkey=1,
-                profile='beyondskins.cerelatino.site:default')
 def to1000(context):
     """ Upgrade from Zero to version 1000
     """
